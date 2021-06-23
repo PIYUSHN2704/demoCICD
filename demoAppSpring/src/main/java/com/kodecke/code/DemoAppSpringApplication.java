fe6a7e314e95e4203b088aad26a81dc8eae2a15f
91981 <91981@DESKTOP-0EH1FSJ>
2021-06-23 11:48:50 +0530

    First Commit

---
+++ demoAppSpring/src/main/java/com/kodecke/code/DemoAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.kodecke.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppSpringApplication.class, args);
+	}
+
+}
---
+++ demoAppSpring/src/main/java/com/kodecke/code/demoHelloWorld.java
@@ -0,0 +1,16 @@
+package com.kodecke.code;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class demoHelloWorld {
+	@RequestMapping("/helloworld")
+	
+	public String helloWorld() {
+		
+		return "Hello World";
+	}
+
+	
+}
